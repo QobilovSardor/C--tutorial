563ac900548e2a24f3976dd55c58ff1b5ff2bd12
SardorQobilov <sardorqobilov555@gmail.com>
2024-12-10 21:45:23 +0500

    c# tutorial

---
+++ MyCSharpApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      // Consolega textni chiqarish
+        Console.WriteLine("Salom dunyo");
+        Console.WriteLine("Davom etish uchun biror tugmani bosing");
+        // Console tugma bosilishini kutadi
+        Console.ReadKey();
+    }
+}
